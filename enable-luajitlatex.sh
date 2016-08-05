1a0e3b9dfb9c617ddabe89368199bd9be18e2b40
Tats.U <tats.u@live.jp>
2016-08-05 22:14:27 +0900

    Initial commit

---
+++ enable-luajitlatex.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Copyright (C) (this script) 2016 Tats.U.
+# This script "itself" is licensed under The MIT License.
+# https://opensource.org/licenses/MIT
+
+if ! which luajittex > /dev/null; then
+    echo 'Error: you must add the executable `luajittex'"'"' to PATH before executing this script.' >&2
+    exit 1
+fi
+if ! kpsewhich fmtutil.cnf > /dev/null; then
+    echo "Error: \`fmtutil.cnf' can't be found." >&2
+    exit 1
+fi
+LUAJITLATEX=`which luajittex | sed 's/tex$/latex/'`
+echo `uname -s` `uname -o` | grep -qi 'm\(ingw\|sys\)' && WINDOWS=1 || WINDOWS=0
+sed -i -e '/luajitlatex/s/^#! //' `kpsewhich fmtutil.cnf` &&
+    fmtutil-sys --byfmt luajitlatex && {
+	[ $WINDOWS = 1 ] ||
+	    cat > $LUAJITLATEX <<"EOF" &&
+#!/bin/sh
+exec $(echo "$0" | sed "s@[^/]\+\$@luajittex@") --fmt=luajitlatex.fmt $*
+EOF
+	chmod +x $LUAJITLATEX
+    } && {
+	echo "Successed to enable LuaJITLaTeX."
+	echo "To use it, execute \`luajitlatex' insted of \`lualatex'."
+    } || {
+	echo "Failed to enable LuaJITLaTeX.">&2
+	echo "Notice: this script may have to be executed by a privileged user.">&2
+    }
